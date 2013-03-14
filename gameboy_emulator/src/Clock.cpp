7f5b316849542c60cacae1ae2f91c7dbca675bdd
Laurence Cartwright <muzoedge@gmail.com>
2013-03-14 11:13:10 +0000

    More thinking, less coffee

--- gameboy_emulator/src/Clock.cpp
+++ gameboy_emulator/src/Clock.cpp
@@ -17,7 +17,7 @@ Clock::~Clock() {
 }
 
 void Clock::cycle(int count){
-	if(count%4 == 0 || count%8==0 || count%12==0 || count%16==0 || count%20==0 || count%24==0){
+	if(count%4 == 0){//I did a bad: ala Nick
 		for(int i=0; i<count; i++){
 				//Do nothing, for now
 		}
